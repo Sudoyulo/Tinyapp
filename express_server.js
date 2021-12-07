a777f53cb555fe0999d59699e24acba735908ec3
Kevin Tan Yulo <yulokevin@gmail.com>
2021-12-06 17:32:48 -0800

    form complete

--- express_server.js
+++ express_server.js
@@ -2,8 +2,15 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,16 +30,27 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new/", (req, res) => {
+
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   //console.log(req.params)
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  const shortURL = req.params.shortURL; //keys
+  const longURL = urlDatabase[shortURL]; //values
   const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
 });
 
+
 app.get("/root/:airplane/:train/:boat", (req,res) => {
-  console.log(req.params); //req params is the url. one param is one :/
+  // console.log(req.params); //req params is the url. one param is one :/
   //saves as an object {airplane: /url link, train:/url/url, boat...}
   res.send("hello"); //body is in the last location (3 files in)
 })
